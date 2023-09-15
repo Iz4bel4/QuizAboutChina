e00d47cff9fb6e5204828cf8560279d7948ddec2
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-09-15 17:22:25 +0200

    Quiz about China, checking the answer, adding scores, moving to another question, final score

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "China has the largest population in the world.", "answer": "True"},
+    {"text": "China is the largest country in the world.", "answer": "False"},
+    {"text": "They have different dialects in China.", "answer": "True"},
+    {"text": "Fish is a symbol of wealth in China.", "answer": "True"},
+    {"text": "The capital of China is Beijing.", "answer": "True"},
+    {"text": "The most popular dish in Beijing is Peking chicken.", "answer": "False"},
+    {"text": "Hong Kong was a British colony.", "answer": "True"},
+    {"text": "They have many political parties in China.", "answer": "False"},
+    {"text": "Chinese flag is red with 5 stars.", "answer": "True"},
+    {"text": "The Chinese invented fireworks.", "answer": "True"},
+    {"text": "China is in the southern hemisphere.", "answer": "False"},
+    {"text": "China is well known from it's one-child policy.", "answer": "True"}
+]
